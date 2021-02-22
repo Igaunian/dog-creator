e022f68e783200f19968a0f2997cf356ab26cdb3
Igaunian <igaunian@gmail.com>
2021-02-22 13:51:02 +0100

    add Dogcreator class

---
+++ src/main/java/com/codecool/dogcreator/DogCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.dogcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogCreatorApplication.class, args);
+    }
+
+}
