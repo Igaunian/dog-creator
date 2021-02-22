95b75dd834bce23a0943b7ee3016cd4e0dfba4da
Igaunian <igaunian@gmail.com>
2021-02-22 14:10:21 +0100

    add logger

--- src/main/java/com/codecool/dogcreator/DogCreatorApplication.java
+++ src/main/java/com/codecool/dogcreator/DogCreatorApplication.java
@@ -1,13 +1,29 @@
 package com.codecool.dogcreator;
 
+import com.codecool.dogcreator.service.DogCreator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
+
 @SpringBootApplication
 public class DogCreatorApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DogCreatorApplication.class);
+
+    @Autowired
+    private DogCreator dogCreator;
 
     public static void main(String[] args) {
         SpringApplication.run(DogCreatorApplication.class, args);
     }
 
+    @PostConstruct
+    public void afterInit() {
+        LOGGER.info(dogCreator.toString());
+    }
 }
